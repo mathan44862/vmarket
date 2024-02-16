aa97a8fa04302beb9fccf0cdcd262fb9d2631d34
Mathan M <mathan44862@gmail.com>
2024-02-16 21:12:04 +0530

    api for login page is updated

---
+++ src/apis/user/index.tsx
@@ -0,0 +1,25 @@
+import {  createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+interface UserLoginDetails{
+    email:String,
+    password:String
+}
+interface ApiResponse {
+    status: string;
+}
+  
+  export const userLogin = createApi({
+    reducerPath: 'userApi',
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+    endpoints: (build) => ({
+      userLogin: build.mutation<ApiResponse,UserLoginDetails>({
+        query: (payload) => ({
+          url: `/login`, 
+          body: payload,
+          method: 'POST'
+        })
+      }),
+    })
+});
+  
+export const {useUserLoginMutation} = userLogin;
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,85 +1,115 @@
-import { Button, Stack, TextField, Typography } from "@mui/material";
+import { Button, Stack, TextField } from "@mui/material";
 import './index.css';
 import { useTypewriter } from "react-simple-typewriter";
+import { useFormik } from "formik";
+import userLoginValidation from "./utils";
+import { useUserLoginMutation } from "../../apis/user";
 
 export default function Login() {
+  const [user,setuser]=useUserLoginMutation();
   const [text] = useTypewriter({
     words: ["From Farm to Fork: Fresh Veggies Delivered to Your Doorstep"],
   });
+  const initialValues = {
+    email : '',
+    password : ''
+  }
+  const {values, handleBlur, handleChange, handleSubmit, errors, touched, isValid, dirty} = useFormik({
+    initialValues: initialValues,
+    validationSchema: userLoginValidation,
+    onSubmit: async (values) => {
+      try {
+        const response = await user(values);
+      } catch (error) {
+         console.error('Unexpected error during sign-in:', error);
+      }
+    }
+  });
   
+
   return (
     <>
-      <h1 style={{ marginLeft: '2%', color: '#a3b18a' }}>{text}</h1>
+      <h3 style={{ marginLeft: '2%', color: '#a3b18a' }}>{text}</h3>
       <Stack marginTop={"8%"} alignItems={"center"} id="background">
         <h4 style={{color:'white'}}>Welcome Back !!</h4><br />
-        <TextField 
-          id="outlined-basic" 
-          label="Email" 
-          variant="outlined" 
-          sx={{
-            width:'40vh',
-            '& .MuiInputLabel-root': { 
-              color: 'white',
-            },
-            '& .MuiOutlinedInput-root': { 
-              '& fieldset': { 
+        <form onSubmit={handleSubmit}>
+          <TextField 
+            id="outlined-basic" 
+            label="Email" 
+            variant="outlined" 
+            sx={{
+              width:'40vh',
+              '& .MuiInputLabel-root': { 
+                color: 'white',
+              },
+              '& .MuiOutlinedInput-root': { 
+                '& fieldset': { 
+                  borderColor: errors.email && touched.email ? 'red' : '#a3b18a',
+                },
+                '&:hover fieldset': {
+                  borderColor: errors.email && touched.email ? 'red' : '#a3b18a',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: errors.email && touched.email ? 'red' : '#a3b18a',
+                },
+              },
+              '& .MuiInputLabel-outlined.Mui-focused': {
+                color: 'white', 
+              },
+            }}
+            name="email"
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          <br /><br />
+          <TextField 
+            id="outlined-basic" 
+            label="Password" 
+            variant="outlined" 
+            sx={{
+              width:'40vh',
+              '& .MuiInputLabel-root': { 
+                color: 'white', 
+              },
+              '& .MuiOutlinedInput-root': { 
+                '& fieldset': { 
+                  borderColor: errors.password && touched.password ? 'red' : '#a3b18a',
+                },
+                '&:hover fieldset': {
+                  borderColor: errors.password && touched.password ? 'red' : '#a3b18a',
+                },
+                '&.Mui-focused fieldset': {
+                  borderColor: errors.password && touched.password ? 'red' : '#a3b18a',
+                },
+              },
+              '& .MuiInputLabel-outlined.Mui-focused': {
+                color: 'white',
+              },
+            }}
+            name="password"
+            type="password"
+            onBlur={handleBlur}
+            onChange={handleChange}
+          />
+          <br /><br />
+          <Button 
+            variant="contained" 
+            sx={{
+              width:'40vh', 
+              color:'#a3b18a', 
+              backgroundColor:'white', 
+              '&:hover': { 
+                backgroundColor: '#a3b18a',
                 borderColor: '#a3b18a', 
-              },
-              '&:hover fieldset': {
-                borderColor: '#a3b18a', 
-              },
-              '&.Mui-focused fieldset': {
-                borderColor: '#a3b18a', 
-              },
-            },
-            '& .MuiInputLabel-outlined.Mui-focused': {
-              color: 'white', 
-            },
-          }}
-        />
-        <br />
-        <TextField 
-          id="outlined-basic" 
-          label="Password" 
-          variant="outlined" 
-          sx={{
-            width:'40vh',
-            color: 'red', // Set text color to red
-            '& .MuiInputLabel-root': { 
-              color: 'white', 
-            },
-            '& .MuiOutlinedInput-root': { 
-              '& fieldset': { 
-                borderColor: '#a3b18a',
-              },
-              '&:hover fieldset': {
-                borderColor: '#a3b18a', 
-              },
-              '&.Mui-focused fieldset': {
-                borderColor: '#a3b18a', 
-              },
-            },
-            '& .MuiInputLabel-outlined.Mui-focused': {
-              color: 'white',
-            },
-          }}
-        />
-        <br />
-        <Button 
-          variant="contained" 
-          sx={{
-            width:'40vh', 
-            color:'#a3b18a', 
-            backgroundColor:'white', 
-            '&:hover': { 
-              backgroundColor: '#a3b18a',
-              borderColor: '#a3b18a', 
-              color: 'white',
-            }
-          }}
-        >
-          Login
-        </Button>
+                color: 'white',
+              }
+            }}
+            type="submit"
+            disabled={!(isValid && dirty)}
+          >
+            Login
+          </Button>
+        </form>
       </Stack>
     </>
   )
